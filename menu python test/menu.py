075035b3d285eb9291b093914bfaba0f0167296c
Eric-LPA <90573052+Eric-LPA@users.noreply.github.com>
2023-01-26 16:31:03 +0100

    Add files via upload

---
+++ menu python test/menu.py
@@ -0,0 +1,121 @@
+from tkinter import *
+import os
+import webbrowser
+import subprocess
+import pathlib
+
+#import music21
+
+
+
+class MyApp:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("My Application")
+        self.window.geometry("1080x1920")
+        self.window.minsize(720, 480)
+        path = os.path.abspath(os.getcwd())
+        test =os.path.join(path, "bg.jpg")
+        #bg = PhotoImage(file = test)
+
+        
+        self.window.config(background='#41B77F')
+
+        # initialization des composants
+        self.frame = Frame(self.window, bg='#41B77F')
+
+        # creation des composants
+        self.create_widgets()
+
+        # empaquetage
+        self.frame.pack(expand=YES)
+
+    def create_widgets(self):
+        self.create_title()
+        self.create_subtitle()
+        self.create_button()
+        self.create_button3()
+        self.create_button2()
+        
+
+    def create_title(self):
+        label_title = Label(self.frame, text="Music for Everyone", font=("Courrier", 40), bg='#41B77F',
+                            fg='white')
+        label_title.pack()
+
+    def create_subtitle(self):
+        label_subtitle = Label(self.frame, text="Naviguate with Tabulations", font=("Courrier", 25), bg='#41B77F',
+                               fg='white')
+        label_subtitle.pack()
+
+    def create_button(self):
+        yt_button = Button(self.frame, text="start the application", font=("Courrier", 25), bg='white', fg='#41B77F',
+                           command=self.open_channel)
+        yt_button.pack(pady=25, fill=X)
+
+    
+
+    def open_channel(self):
+        #webbrowser.open_new("http://youtube.com")
+        music_1 = r'"C:\Program Files (x86)\MuseScore 3\bin\MuseScore3.exe" "C:\Users\EN_Le\OneDrive\Bureau\PFE\menu python test\Passacaglia.mscz"'
+        
+        
+        os.popen(music_1)
+        #subprocess.call([r"C:\Program Files (x86)\MuseScore 3\bin\MuseScore3.exe 'C:\Users\EN_Le\OneDrive\Bureau\PFE\menu python test\Passacaglia.mscz'"])
+        #c = music21.converter.parse('path_to_musicxml.xml')
+        #c.show('musicxml.pdf')
+        #c.show('midi')
+
+    def create_button3(self):
+        yt_button3 = Button(self.frame, text="Oral Instructions", font=("Courrier", 25), bg='white', fg='#41B77F',
+                       command=self.open_channel3)
+        yt_button3.pack(pady=45, fill=X)
+
+
+
+
+    def open_channel3(self):
+        music_1 = r'"C:\Program Files (x86)\MuseScore 3\bin\MuseScore3.exe" "C:\Users\EN_Le\OneDrive\Bureau\PFE\menu python test\Passacaglia.mscz"'
+        
+        
+        os.popen(music_1)
+
+
+
+    def create_button2(self):
+        yt_button2 = Button(self.frame, text="select a music sheet", font=("Courrier", 25), bg='white', fg='#41B77F',
+                           command=self.open_channel2)
+        yt_button2.pack(pady=45, fill=X)
+
+    def open_channel2(self):
+        
+
+        liste = Listbox(self.frame)
+        liste.insert(1, "music 1")
+        liste.insert(2, "music 2")
+        liste.insert(3, "music 3")
+        liste.insert(4, "music 4")
+        liste.insert(5, "music 5")
+        liste.insert(6, "music 6")
+
+        liste.pack()
+        
+        
+
+            
+
+
+
+
+
+
+        
+        
+
+
+
+
+# afficher
+app = MyApp()
+app.window.mainloop()
